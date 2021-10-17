590efb2d714fed3393d8691f9e17eb20a67da362
mlodyFrontendowiec <pawel.kruszelnicki@onet.pl>
2021-10-17 17:21:16 +0200

    Fetch data from API

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,33 @@
+import axios from "axios";
+
 const form = document.querySelector("form")! as HTMLFormElement;
 const addressInput = document.getElementById("address")! as HTMLInputElement;
 
+const GOOGLE_API_KEY = "AIzaSyCIaAc2c5M3VpbCH6PPq_guwy9lHuowXOs";
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: "OK" | "ZERO_RESULTS";
+};
 function searchAddresHandler(event: Event) {
   event.preventDefault();
   const enteredAddress = addressInput?.value;
+
+  axios
+    .get<GoogleGeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${GOOGLE_API_KEY}`
+    )
+    .then((res) => {
+      if (res.data.status !== "OK") {
+        throw new Error("Could not fetch location!");
+      }
+      const coordinates = res.data.results[0].geometry.location;
+    })
+    .catch((err) => {
+      alert(err.message);
+      console.log(err);
+    });
 }
 
 form?.addEventListener("submit", searchAddresHandler);
