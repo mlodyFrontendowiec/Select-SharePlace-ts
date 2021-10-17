0b5894781f40d08ea1cb1b271c9798730012e710
mlodyFrontendowiec <pawel.kruszelnicki@onet.pl>
2021-10-17 16:41:06 +0200

    Get elements from DOM

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,9 @@
-// Code goes here!
+const form = document.querySelector("form")! as HTMLFormElement;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
+
+function searchAddresHandler(event: Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput?.value;
+}
+
+form?.addEventListener("submit", searchAddresHandler);
